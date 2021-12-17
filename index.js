0a3ab8fc8dd41696cb427e254c10602da57da868
C1200 <coreyw.ultimatemedia@gmail.com>
2021-12-17 16:10:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+const { Command } = require("commander");
+const Zip = require("adm-zip");
+const fetch = require("node-fetch");
+const path = require("path");
+const fs = require("fs-extra");
+const { pipeline } = require("stream");
+const { exec } = require("child_process");
+const chalk = require("chalk");
+const updateNotifier = require('update-notifier');
+const pkg = require("./package.json");
+
+const program = new Command();
+
+var update = updateNotifier({pkg});
+
+program
+    .name(pkg.name)
+    .version(pkg.version);
+
+program
+  .command("create [destination]")
+  .description("Create a ChatSurf server")
+  .action(async (destination) => {
+    var root = path.resolve(destination || "chatsurf");
+    
+    var res = await fetch("https://github.com/chatsurfapp/chatsurf/zipball/main");
+    
+    fs.mkdirSync(root + "-tmp");
+    
+    pipeline(res.body, fs.createWriteStream(path.join(root + "-tmp", "chatsurf.zip")), (err) => {
+      if (err) throw err;
+      
+      var zip = new Zip(path.join(root + "-tmp", "chatsurf.zip"));
+      zip.extractAllTo(root + "-tmp");
+      
+      fs.move(path.join(root + "-tmp", zip.getEntries()[0].entryName), root, (err) => {
+        if (err) throw err;
+        
+        fs.removeSync(root + "-tmp");
+        
+        exec("npm install", {
+          cwd: root
+        }).on("exit", (code) => {
+          if (code > 0) throw new Error(`Command 'npm install' exited with code ${code}`);
+          
+          console.log(chalk.green(`Created ChatSurf server in: ${chalk.blue(root)}!`));
+          console.log(chalk.green("\nNext Steps:\nStart your server:"));
+          console.log(chalk.blue(` $ cd ${root}\n $ npm start`));
+          
+          update.notify();
+        });
+      });
+    });
+  });
+
+program
+  .command("install <plugin>")
+  .description("Install a plugin")
+  .action(async (plugin) => {
+    var root = path.resolve("plugins");
+    fs.ensureDirSync(root);
+  
+    var res = await fetch(`https://chatsurfapp.github.io/plugins/${plugin}.js`);
+  
+    if (res.ok) {
+      pipeline(res.body, fs.createWriteStream(path.join(root, `${plugin}.js`)), (err) => {
+        if (err) throw err;
+        
+        console.log(chalk.green(`Installed plugin: ${chalk.blue(plugin)}`));
+      });
+    }
+  });
+
+program.parse(process.argv);
